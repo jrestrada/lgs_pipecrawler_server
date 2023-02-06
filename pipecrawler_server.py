4b9ddf3c4f7ef9d20d28e67fb2b682f8b5db34d4
jrestrada <jramiroem@gmail.com>
2023-02-06 16:36:18 -0500

    added response

--- pipecrawler_server.py
+++ pipecrawler_server.py
@@ -2,26 +2,23 @@
 
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Description: :))) ;)
+Description:
 Action Server that controls a pipe crawler's pneumatics
 through raspberry pi pins, relay, and solenoid valves
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Test with: 
 
-ros2 action send_goal --feedback crawl_through_pipe pipecrawler/action/Crawlaction '{crawlercommand: {crawlpattern: [2,5,3,6,1,4], continuous: True}}'
-ros2 action send_goal --feedback crawl_through_pipe pipecrawler/action/Crawlaction '{crawlercommand: {crawlpattern: [0,0,0,0,0,0], continuous: False}}'
+ros2 action send_goal --feedback crawler_action pipecrawler/action/Crawleraction '{command: {pattern: [2,5,3,6,1,4], timing: [,,,,,,,], looping: True}}'
+ros2 action send_goal --feedback crawler_action pipecrawler/action/Crawleraction '{command: {pattern: [0,0,0,0], looping: False}}'
 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Subscription Topics:
-    pipecrawler/Crawlpattern
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 import threading
 from argparse import Action
 from os import pipe
 import time
 import rclpy
-from pipecrawler.action import Crawlaction
+from pipecrawler.action import Crawleraction
+from pipecrawler.msg import Crawlerstate    
 from rclpy.action import ActionServer
 from rclpy.action import CancelResponse
 from rclpy.action import GoalResponse
@@ -62,16 +59,10 @@ gripper_commands_consolever = {
     0:none
 }
 
-# d = 1
-
 d = 2.95
 gf = gpio(5)
 e = gpio(6)
 gb = gpio(16)
-
-#Use following when using control box 
-
-#Black mark = TOP
 
 gripper_commands = {
     1:gb.on, #GREEN 
@@ -83,27 +74,22 @@ gripper_commands = {
     0:none
 }
 
-def executeCommands(command_list):
-    for i in range(len(command_list)):
-            gripper_commands[command_list[i]]()
-            time.sleep(d)
-
-class CrawlactionServer(Node):
+class CrawleractionServer(Node):
     def __init__(self):
         super().__init__('pipecrawler_server')  # Node instance name ()must be matched)
         self.get_logger().info('Initializing Pipecrawler Server')
-        self._crawl_counter = 0
         self._goal_lock = threading.Lock()
         self._goal_handle = None
+        self._state = [0,0,0]
         self._action_server = ActionServer(
             self,                               # Node
-            Crawlaction,                        # Action Type, imported
-            'crawl_through_pipe',               # Action Name (Must be matched in other nodes or commands)
-            execute_callback =self.execute_callback,        # Callback Function to execute
+            Crawleraction,                        # Action Type, imported
+            'crawler_action',               # Action Name (Must be matched in other nodes or commands)
+            execute_callback = self.execute_callback,        # Callback Function to execute
             goal_callback = self.goal_callback,
-            handle_accepted_callback=self.handle_accepted_callback,
-            cancel_callback=self.cancel_callback,
-            callback_group=ReentrantCallbackGroup())
+            handle_accepted_callback = self.handle_accepted_callback,
+            cancel_callback =self.cancel_callback,
+            callback_group = ReentrantCallbackGroup())
         
     def handle_accepted_callback(self, goal_handle):
         with self._goal_lock:
@@ -116,6 +102,15 @@ class CrawlactionServer(Node):
 
         goal_handle.execute()
 
+    def update(self, command):
+        self._state[((command+1)//2)-1] = command
+
+    def execute(self, command_list, timing_list):
+        for i in range(len(command_list)):
+            gripper_commands[command_list[i]]()
+            self.update(command_list[i])
+            time.sleep(timing_list[i])
+
     def goal_callback(self, goal_request):
         """Accept or reject a client request to begin an action."""
         self.get_logger().info('Received goal request')
@@ -127,41 +122,41 @@ class CrawlactionServer(Node):
         return CancelResponse.ACCEPT
 
     def execute_callback(self, goal_handle):
-        current_commands_list = goal_handle.request.crawlercommand.crawlpattern
+        _pattern = goal_handle.request.command.pattern
+        _timing = goal_handle.request.command.timing
 
-        if goal_handle.request.crawlercommand.continuous:
-            self.get_logger().info("Executing continuous crawl command")
+        if goal_handle.request.command.looping:
+            self.get_logger().info("Executing looping crawl command")
             while goal_handle.is_active:
                 
                 if goal_handle.is_cancel_requested:
                     goal_handle.canceled()
                     self.get_logger().info('Goal canceled')
                 
-                executeCommands(current_commands_list)
-                self._crawl_counter += 1
-                feedback_msg = Crawlaction.Feedback()
-                feedback_msg.count = self._crawl_counter
-                goal_handle.publish_feedback(feedback_msg)
+                self.execute(_pattern,_timing)
+                feedback_ = Crawleraction.Feedback()
+                feedback_.current.state = self._state
+                goal_handle.publish_feedback(feedback_)
                               
         else: 
             self.get_logger().info("Executing single command")
-            executeCommands(current_commands_list)
-            feedback_msg = Crawlaction.Feedback()
-            feedback_msg.count = self._crawl_counter
-            goal_handle.publish_feedback(feedback_msg)
+            self.execute(_pattern,_timing)
+            feedback_ = Crawleraction.Feedback()
+            goal_handle.publish_feedback(feedback_)
             self.get_logger().info("Goal Succeeded")
             goal_handle.succeed()
         
-        resultant = Crawlaction.Result()
-        resultant.successfulcrawl = True
-        return resultant
+        result_ = Crawleraction.Result()
+        result_.final.state = self._state
+        result_.success = True
+        return result_
 
 def main(args=None):
     rclpy.init(args=args)
    
     try: 
         # Create the Action Server Node instance
-        pipecrawler_server_instance = CrawlactionServer()
+        pipecrawler_server_instance = CrawleractionServer()
         
         # Set up mulithreading
         executor = MultiThreadedExecutor(num_threads=4)
